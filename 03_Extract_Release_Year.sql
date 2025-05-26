c0b4cac88f9d751e4095fa18d7be392cc99089b0
Areej <137380525+Areej20025@users.noreply.github.com>
2025-05-26 21:52:03 +0300

    Add files via upload

---
+++ 03_Extract_Release_Year.sql
@@ -0,0 +1,20 @@
+﻿
+
+ALTER TABLE copy_movies
+ADD release_year INT;
+
+
+UPDATE copy_movies
+SET release_year = CAST(SUBSTRING(title, LEN(title) - 4, 4) AS INT)
+WHERE title LIKE '%(____)';  -- تأكد أن فيها أربع أرقام بين قوسين
+
+
+
+UPDATE copy_movies
+SET title = RTRIM(LEFT(title, LEN(title) - 7))  -- يحذف آخر 7 حروف (يشمل الفراغ قبل القوس)
+WHERE title LIKE '%(____)';
+
+
+SELECT * 
+FROM copy_movies
+where release_year ='2002';
